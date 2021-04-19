2b4305761678f3256c300906f7ee3734e4638f93
ThomasKubant <77358241+ThomasKubant@users.noreply.github.com>
2021-04-18 22:06:39 -0700

    finished

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 const shortid = require('shortid');
 const { notes } = require('../../db/db.json');
 const { saveNote, validateNote, findById, deleteNote } = require('../../lib/notes');
-router.get('./api/notes', (req, res) => {
+router.get('/notes', (req, res) => {
     let results = notes;
     res.json(results);
 });
-router.post('./api/notes', (req, res) => {
+router.post('/notes', (req, res) => {
     req.body.id = shortid.generate();
     if(!validateNote(req.body)) {
         res.status(400).send("Notes need a name and body text.");
@@ -15,7 +15,7 @@ router.post('./api/notes', (req, res) => {
         res.json(note);
    }
 })
-router.post('./api/notes/:id', (req, res) => {
+router.post('/notes/:id', (req, res) => {
     const result = filterById(req.params.id, notes);
     if(result) {
         res.json(result);
@@ -24,7 +24,7 @@ router.post('./api/notes/:id', (req, res) => {
         res.send(404);
    }
 })
-router.delete('./api/notes/:id', (req, res) => {
+router.delete('/notes/:id', (req, res) => {
     const result = deleteNote(req.params.id, notes);
     res.json(result);
 })
